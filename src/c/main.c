e01e2fbcb3d09768607e9cb4992d944175d1c0a3
casainho <casainho@gmail.com>
2016-11-23 18:03:18 +0000

    Added background

--- src/c/main.c
+++ src/c/main.c
@@ -1,142 +1,69 @@
 #include "pebble.h"
+#include "pebble_fonts.h"
 #include "main.h"
 
-static Window *s_window;
-static Layer *s_simple_bg_layer, *s_hands_layer;
-static TextLayer *s_calories_label;
+static Window *s_main_window;
+static Layer *s_background1_layer;
+static Layer *s_background2_layer;
 
-static GPath *s_tick_paths[NUM_CLOCK_TICKS];
-static GPath *s_minute_arrow, *s_hour_arrow;
+static void background1_layer_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(s_background1_layer);
 
-static void bg_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    const int x_offset = PBL_IF_ROUND_ELSE(18, 0);
-    const int y_offset = PBL_IF_ROUND_ELSE(6, 0);
-    gpath_move_to(s_tick_paths[i], GPoint(x_offset, y_offset));
-    gpath_draw_filled(ctx, s_tick_paths[i]);
-  }
-}
-
-static void hands_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
-  GPoint center = grect_center_point(&bounds);
-
-  const int16_t second_hand_length = PBL_IF_ROUND_ELSE((bounds.size.w / 2) - 19, bounds.size.w / 2);
-
-  time_t now = time(NULL);
-  struct tm *t = localtime(&now);
-  int32_t second_angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
-  GPoint second_hand = {
-    .x = (int16_t)(sin_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.x,
-    .y = (int16_t)(-cos_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.y,
-  };
-
-  // second hand
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_draw_line(ctx, second_hand, center);
-
-  // minute/hour hand
-  graphics_context_set_fill_color(ctx, GColorWhite);
+  // Draw the black background
   graphics_context_set_stroke_color(ctx, GColorBlack);
-
-  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-  gpath_draw_filled(ctx, s_minute_arrow);
-  gpath_draw_outline(ctx, s_minute_arrow);
-
-  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
-  gpath_draw_filled(ctx, s_hour_arrow);
-  gpath_draw_outline(ctx, s_hour_arrow);
-
-  // dot in the middle
   graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
-  static char s_value_buffer[8];
-  int cals = health_service_sum_today(HealthMetricRestingKCalories) + health_service_sum_today(HealthMetricActiveKCalories);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "cals = %d", cals);
-  cals = 101;
-  snprintf(s_value_buffer, sizeof(s_value_buffer), "%d", cals);
-  text_layer_set_text(s_calories_label, s_value_buffer);
+  // Draw the white rectangle with round corners
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_stroke_width(ctx, 0);
+  graphics_fill_rect(ctx, bounds, 12, GCornersAll);
 }
 
-static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(window_get_root_layer(s_window));
-}
-
-static void window_load(Window *window) {
+static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_simple_bg_layer = layer_create(bounds);
-  layer_set_update_proc(s_simple_bg_layer, bg_update_proc);
-  layer_add_child(window_layer, s_simple_bg_layer);
+  // Properly create a full-screen Layer
+  s_background1_layer = layer_create(bounds);
+  s_background2_layer = layer_create(bounds);
 
-  s_hands_layer = layer_create(bounds);
-  layer_set_update_proc(s_hands_layer, hands_update_proc);
-  layer_add_child(window_layer, s_hands_layer);
+  // Assign the custom drawing procedure
+  layer_set_update_proc(s_background1_layer, background1_layer_update_proc);
 
-  s_calories_label = text_layer_create(GRect(0, 0, 58, 20));
-  text_layer_set_background_color(s_calories_label, GColorWhite);
-  text_layer_set_text_color(s_calories_label, GColorBlack);
-  text_layer_set_font(s_calories_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text_alignment(s_calories_label, GTextAlignmentCenter);
+  // Redraw this as soon as possible
+  layer_mark_dirty(s_background1_layer);
+
+  // Add to the Window
+  layer_add_child(window_layer, s_background1_layer);
+  layer_add_child(window_layer, s_background2_layer);
 }
 
-static void window_unload(Window *window) {
-  health_service_events_unsubscribe();
-
-  layer_destroy(s_simple_bg_layer);
-
-  text_layer_destroy(s_calories_label);
-
-  layer_destroy(s_hands_layer);
+static void main_window_unload(Window *window) {
+  // Destroy Layer
+  layer_destroy(s_background1_layer);
+  layer_destroy(s_background2_layer);
 }
 
 static void init() {
   // Create main Window element and assign to pointer
-  s_window = window_create();
+  s_main_window = window_create();
 
   // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
   });
 
   // Show the Window on the watch, with animated=true
-  window_stack_push(s_window, true);
-
-  // init hand paths
-  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
-  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
-
-  Layer *window_layer = window_get_root_layer(s_window);
-  GRect bounds = layer_get_bounds(window_layer);
-  GPoint center = grect_center_point(&bounds);
-  gpath_move_to(s_minute_arrow, center);
-  gpath_move_to(s_hour_arrow, center);
-
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
-  }
-
-  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
-  gpath_destroy(s_minute_arrow);
-  gpath_destroy(s_hour_arrow);
-
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    gpath_destroy(s_tick_paths[i]);
-  }
-
-  tick_timer_service_unsubscribe();
-
   // Destroy Window
-  window_destroy(s_window);
+  window_destroy(s_main_window);
 }
 
 int main() {
