71129abbd24c1847778fec1151bceb1262bdd173
casainho <casainho@gmail.com>
2016-11-27 17:13:56 +0000

    Works with background as image and hands with real time render

--- src/c/main.c
+++ src/c/main.c
@@ -5,47 +5,50 @@
 static Window *s_main_window;
 static GBitmap *s_bitmap;
 static BitmapLayer *s_background_layer;
-static RotBitmapLayer *s_hours_hand_layer;
-static RotBitmapLayer *s_minutes_hand_layer;
+static GPath *s_minutes_handle_path;
+static GPath *s_hours_handle_path;
+static Layer *s_hands_layer;
 
-static void handleTick(struct tm *t, TimeUnits units_changed) {
-  GRect rectangle;
-  int32_t minute_angle = t->tm_sec * TRIG_MAX_ANGLE / 60;
-  int32_t hour_angle = ((t->tm_min%12)*60 + t->tm_sec) * TRIG_MAX_ANGLE / (12 * 60);
+static void hands_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
 
+  const int16_t second_hand_length = PBL_IF_ROUND_ELSE((bounds.size.w / 2) - 19, bounds.size.w / 2);
 
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  int32_t second_angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
+  GPoint second_hand = {
+    .x = (int16_t)(sin_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.y,
+  };
 
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "rectangle.size.h: %d", rectangle.size.h);
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "rectangle.size.w: %d", rectangle.size.w);
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "rectangle.origin.x: %d", rectangle.origin.x);
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "rectangle.origin.y: %d", rectangle.origin.y);
-//
-//  rectangle = layer_get_frame((Layer *)s_minutes_hand_layer);
-//  rectangle.origin.x = (PBL_DISPLAY_WIDTH_HALF - rectangle.size.w/2);
-//  rectangle.origin.y = PBL_DISPLAY_HEIGHT_HALF - 68 + 6;
-//
-//  rectangle.size.w = rectangle.size.w + rectangle.size.w;
-//  rectangle.size.h = rectangle.size.h + rectangle.size.h;
-//
-//  layer_set_frame((Layer *)s_minutes_hand_layer, rectangle);
+  // second hand
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_line(ctx, second_hand, center);
 
-  rot_bitmap_layer_set_angle(s_minutes_hand_layer, minute_angle);
+  // minute/hour hand
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, GColorWhite);
 
-//  rectangle.origin.x = PBL_DISPLAY_WIDTH_HALF - rectangle.size.w/2 + 56 * cos_lookup((minuteAngle + 3 * TRIG_MAX_ANGLE / 4)%TRIG_MAX_ANGLE) / TRIG_MAX_RATIO;
-//  rectangle.origin.y = PBL_DISPLAY_HEIGHT_HALF - rectangle.size.h/2 + 56 * sin_lookup((minuteAngle + 3 * TRIG_MAX_ANGLE / 4)%TRIG_MAX_ANGLE) / TRIG_MAX_RATIO;
-//  layer_set_frame((Layer *)minuteHandLayer, r);
-//  rot_bitmap_layer_set_angle(minuteHandLayer, minuteAngle);
-//
-//  rectangle = layer_get_frame((Layer *)hourHandLayer);
-//  rectangle.origin.x = 72 - rectangle.size.w/2 + 57 * cos_lookup((hourAngle + 3 * TRIG_MAX_ANGLE / 4)%TRIG_MAX_ANGLE) / TRIG_MAX_RATIO;
-//  rectangle.origin.y = 84 - rectangle.size.h/2 + 57 * sin_lookup((hourAngle + 3 * TRIG_MAX_ANGLE / 4)%TRIG_MAX_ANGLE) / TRIG_MAX_RATIO;
-//  layer_set_frame((Layer *)hourHandLayer, r);
-//  rot_bitmap_layer_set_angle(hourHandLayer, hourAngle);
+  gpath_rotate_to(s_minutes_handle_path, TRIG_MAX_ANGLE * t->tm_min / 60);
+  gpath_draw_filled(ctx, s_minutes_handle_path);
+  gpath_draw_outline(ctx, s_minutes_handle_path);
+
+  gpath_rotate_to(s_hours_handle_path, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
+  gpath_draw_filled(ctx, s_hours_handle_path);
+  gpath_draw_outline(ctx, s_hours_handle_path);
+
+  // dot in the middle
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
+}
+
+static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(window_get_root_layer(s_main_window));
 }
 
 static void push_background (Window *window) {
-  time_t t = time(NULL);
-
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -53,32 +56,22 @@ static void push_background (Window *window) {
   // Prepare background image layer
   s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
   s_background_layer = bitmap_layer_create(GRect(0, 0, PBL_DISPLAY_WIDTH, PBL_DISPLAY_HEIGHT));
-  //bitmap_layer_set_compositing_mode(s_background_layer, GCompOpAssign);
   bitmap_layer_set_bitmap(s_background_layer, s_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
-  // Prepare  minutes hand layer
-  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_MINUTES_HAND);
-  s_minutes_hand_layer = rot_bitmap_layer_create(s_bitmap);
-  rot_bitmap_set_compositing_mode(s_minutes_hand_layer, GCompOpSet);
+  // Init hand paths
+  s_minutes_handle_path = gpath_create(&MINUTE_HAND_POINTS);
+  s_hours_handle_path = gpath_create(&HOUR_HAND_POINTS);
 
-  GRect rectangle = layer_get_frame((Layer *)s_minutes_hand_layer);
-  rectangle.origin.x = (PBL_DISPLAY_WIDTH_HALF - rectangle.size.w/2);
-  rectangle.origin.y = PBL_DISPLAY_HEIGHT_HALF - 68 + 6;
-  layer_set_frame((Layer *)s_minutes_hand_layer, rectangle);
+  GPoint center = grect_center_point(&bounds);
+  gpath_move_to(s_minutes_handle_path, center);
+  gpath_move_to(s_hours_handle_path, center);
 
-  layer_add_child(window_get_root_layer(window), (Layer *)s_minutes_hand_layer);
+  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 
-  // Prepare  hour hand layer
-  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_HOURS_HAND);
-  s_hours_hand_layer = rot_bitmap_layer_create(s_bitmap);
-  rot_bitmap_set_compositing_mode(s_hours_hand_layer, GCompOpSet);
-//  layer_add_child(window_get_root_layer(window), (Layer *)s_hours_hand_layer);
-
-  // Draw the handles now
-  handleTick(localtime(&t), SECOND_UNIT|MINUTE_UNIT|HOUR_UNIT);
-
-  tick_timer_service_subscribe(SECOND_UNIT, handleTick);
+  s_hands_layer = layer_create(bounds);
+  layer_set_update_proc(s_hands_layer, hands_update_proc);
+  layer_add_child(window_layer, s_hands_layer);
 }
 
 static void main_window_load(Window *window) {
@@ -89,8 +82,6 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   gbitmap_destroy(s_bitmap);
   bitmap_layer_destroy(s_background_layer);
-  rot_bitmap_layer_destroy(s_hours_hand_layer);
-  rot_bitmap_layer_destroy(s_minutes_hand_layer);
 }
 
 static void init() {
--- src/c/main.h
+++ src/c/main.h
@@ -6,3 +6,19 @@
 
 #define PBL_DISPLAY_WIDTH_HALF PBL_DISPLAY_WIDTH / 2
 #define PBL_DISPLAY_HEIGHT_HALF PBL_DISPLAY_HEIGHT / 2
+
+static const GPathInfo MINUTE_HAND_POINTS = {
+  3, (GPoint []) {
+    { -8, 20 },
+    { 8, 20 },
+    { 0, -80 }
+  }
+};
+
+static const GPathInfo HOUR_HAND_POINTS = {
+  3, (GPoint []){
+    {-6, 20},
+    {6, 20},
+    {0, -60}
+  }
+};
